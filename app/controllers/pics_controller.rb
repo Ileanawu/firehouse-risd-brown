1aa019091ed7457c69c29b308dc7023d3fe342f9
Firehose <firehose@marcos-laptop.risd.edu>
2014-02-08 17:11:02 -0500

    adding form

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,4 +4,21 @@ class PicsController < ApplicationController
 		@pics = Pic.all
 	end	
 
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		Pic.create(pics_params)
+		redirect_to pics_path
+	end 	
+
+	private
+
+	def pics_params
+		params.require(:pic).permit(
+			:hour, :emotion, :learned
+		)
+	end	
+
 end
